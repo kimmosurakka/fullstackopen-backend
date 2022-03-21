977379812dda7adce6aa303362f5a9bb795b250f
Kimmo Surakka <kimmo.surakka@gmail.com>
2022-03-21 14:19:26 +0200

    3.3 puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ app.get('/info', (request, response) => {
   )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log('id:', id, typeof id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
