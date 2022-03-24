01dae2fe802d66e667cdc6f4062ad44b99c4db2e
Kimmo Surakka <kimmo.surakka@gmail.com>
2022-03-24 15:45:53 +0200

    3.10 puhelinluettelon backend step10

---
+++ README.md
@@ -0,0 +1,5 @@
+# Puhelinluettelo backend
+
+This is the phone book backend for the full stack open course.
+
+The application is running in Heroku, URL https://puhelimet.herokuapp.com/
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 let persons = [
   {
