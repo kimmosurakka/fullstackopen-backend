b96b4bf72e10a31781280d21be7457c6cda1ca54
Kimmo Surakka <kimmo.surakka@gmail.com>
2023-12-05 13:40:02 +0200

    Refactoring bloglist, step 4: app module

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,18 +1,5 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const app = require('./app')
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
