264e3cd106243582b692838448ea750d4c22bb5e
Kimmo Surakka <kimmo.surakka@kipina.fi>
2023-12-05 11:42:32 +0200

    Refactoring bloglist, step 1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,21 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
