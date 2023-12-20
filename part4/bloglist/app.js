9e76bc97477ef4127a9d0886fa199a170c254fc5
Kimmo Surakka <kimmo.surakka@gmail.com>
2023-12-20 14:32:59 +0200

    Useful middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -15,9 +15,11 @@ mongoose.connect(config.MONGODB_URI).then(
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,5 +1,17 @@
 const logger = require('./logger')
 
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:', request.path)
+  logger.info('Body:', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -11,4 +23,8 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { errorHandler }
+module.exports = {
+  errorHandler,
+  requestLogger,
+  unknownEndpoint
+}
